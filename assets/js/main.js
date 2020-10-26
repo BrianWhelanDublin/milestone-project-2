198192040025b6730ec2e597dc85914d6f48e41a
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2020-10-26 12:15:59 +0000

    created code to animat on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,31 @@
-/* Code to open and close the navigation menu*/ 
+/* Code to open and close the navigation menu*/
 
 let menu = document.querySelector(".hamburger");
 let navigation = document.querySelector(".navigation");
-let link = document.querySelectorAll(".navigation-item")
+let link = document.querySelectorAll(".navigation-item");
 
 menu.addEventListener("click", () => {
+  navigation.classList.toggle("change");
+});
+
+link.forEach((item) => {
+  item.addEventListener("click", () => {
     navigation.classList.toggle("change");
- });
+  });
+});
 
- link.forEach(item=>{
-     item.addEventListener("click", ()=>{
-     navigation.classList.toggle("change")
-    })
- })
+/* Code to fade elements in upon scrolling */
 
+const animatuionOnScroll = () => {
+  let animationElement = document.querySelectorAll(".animation-element");
+  animationElement.forEach((element) => {
+    let position = element.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight / 1.5;
 
+    if (position < screenPosition) {
+      element.classList.add("animation-active");
+    }
+  });
+};
+
+window.addEventListener("scroll", animationOnScroll);
