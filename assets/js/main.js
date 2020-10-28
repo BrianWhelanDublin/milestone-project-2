e1ed5d7729b5e998bbe12a4b28c5b6d59860d736
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2020-10-28 14:35:53 +0000

    edited controls

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,54 +5,100 @@ let navigation = document.querySelector(".navigation");
 let link = document.querySelectorAll(".navigation-item");
 
 menu.addEventListener("click", () => {
-  navigation.classList.toggle("change");
+    navigation.classList.toggle("change");
 });
 
 /* Adds an event listener to each navigation item to close the navigation menu when the link is clicked*/
 link.forEach((item) => {
-  item.addEventListener("click", () => {
-    navigation.classList.toggle("change");
-  });
+    item.addEventListener("click", () => {
+        navigation.classList.toggle("change");
+    });
 });
 
 /* Code to fade elements in upon scrolling */
 
 const animationOnScroll = () => {
-  let animationElement = document.querySelectorAll(".animation-element");
-  animationElement.forEach((element) => {
-    let position = element.getBoundingClientRect().top;
-    let screenPosition = window.innerHeight / 1.4;
-    if (position < screenPosition) {
-      element.classList.add("animation-active");
-    }
-  });
+    let animationElement = document.querySelectorAll(".animation-element");
+    animationElement.forEach((element) => {
+        let position = element.getBoundingClientRect().top;
+        let screenPosition = window.innerHeight / 1.4;
+        if (position < screenPosition) {
+            element.classList.add("animation-active");
+        }
+    });
 };
 
 window.addEventListener("scroll", animationOnScroll);
 
 /* Destinations */
 
-let cityDestination = [
-  {id="c0", name: "berlin", image: "../images/cities/berlin.jpg"},
-  {id="c1", name: "dubai", image: "../images/cities/dubai.jpg"},
-  {id="c2",name: "kyoto", image: "../images/cities/kyoto.jpg"},
-  {id="c3",name: "london", image: "../images/cities/london.jpg"},
-  {id="c4",name: "madrid", image: "../images/cities/madrid.jpg"},
-  {id="c5",name: "new york", image: "../images/cities/new-york.jpg"},
-  {id="c6",name: "paris", image: "../images/cities/paris.jpg"},
-  {id="c7",name: "san francisco", image: "../images/cities/san-francisco.jpg"},
-  {id="c8",name: "shanghai", image: "../images/cities/shanghai.jpg"},
-  {id="c10",name: "singapore", image: "../images/cities/singapore.jpg"},
-  {id="c11",name: "tokyo", image: "../images/cities/tokyo.jpg"}
+let cityDestinations = [
+    { id: "c0", name: "berlin", image: "assets/images/cities/berlin.jpg" },
+    { id: "c1", name: "dubai", image: "assets/images/cities/dubai.jpg" },
+    { id: "c2", name: "kyoto", image: "assets/images/cities/kyoto.jpg" },
+    { id: "c3", name: "london", image: "assets/images/cities/london.jpg" },
+    { id: "c4", name: "madrid", image: "assets/images/cities/madrid.jpg" },
+    { id: "c5", name: "new york", image: "assets/images/cities/new-york.jpg" },
+    { id: "c6", name: "paris", image: "assets/images/cities/paris.jpg" },
+    { id: "c7", name: "san francisco", image: "assets/images/cities/san-francisco.jpg" },
+    { id: "c8", name: "shanghai", image: "assets//images/cities/shanghai.jpg" },
+    { id: "c10", name: "singapore", image: "assets/images/cities/singapore.jpg"},
+    { id: "c11", name: "tokyo", image: "assets/images/cities/tokyo.jpg" },
 ];
-let beachDestinations =[
-     {id="b0", name: "bali", image: "../images/beaches/beaches.jpg"},
-     {id="b1", name: "barbados", image: "../images/beaches/barbados.jpg"},
-     {id="b2", name: "cannes", image: "../images/beaches/cannes.jpg"},
-     {id="b3", name: "capri", image: "../images/beaches/capri.jpg"},
-     {id="b4", name: "cinqu terre", image: "../images/beaches/cinque-terre.jpg"},
-     {id="b5", name: "koh phi phi", image: "../images/beaches/koh-phi-phi.jpg"},
-     {id="b6", name: "maldives", image: "../images/beaches/maldives.jpg"},
-     {id="b7", name: "mauritius", image: "../images/beaches/mauritius.jpg"},
-     {id="b8", name: "santorini", image: "../images/beaches/santorini.jpg"}
-]
+let beachDestinations = [
+    { id: "b0", name: "bali", image: "assets/images/beaches/bali.jpg" },
+    { id: "b1", name: "barbados", image: "assets/images/beaches/barbados.jpg" },
+    { id: "b2", name: "cannes", image: "assets/images/beaches/cannes.jpg" },
+    { id: "b3", name: "capri", image: "assets/images/beaches/capri.jpg" },
+    { id: "b4", name: "cinqu terre", image: "assets/images/beaches/cinque-terre.jpg" },
+    { id: "b5", name: "koh phi phi", image: "assets/images/beaches/koh-phi-phi.jpg" },
+    { id: "b6", name: "maldives", image: "assets/images/beaches/maldives.jpg" },
+    { id: "b7", name: "mauritius", image: "assets/images/beaches/mauritius.jpg" },
+    { id: "b8", name: "santorini", image: "assets/images/beaches/santorini.jpg" },
+];
+
+/* Renders li elements from destinations array's */
+
+const renderDestination = (destination, sliderName) => {
+    const markup = `
+    <li class="card">
+      <img src="${destination.image}" alt="" class="card-img">
+        <div class="card-content">
+          <h2 class="card-title">${destination.name}</h2>
+          <button class="card-btn">Find Out More</button>
+        </div>
+      <div class="gradient"></div>
+    </li>
+    `;
+    document.getElementById(sliderName).insertAdjacentHTML("beforeend", markup);
+};
+
+/* Literates through destination list and for each destination renders a li element */
+
+const getDestinationElement = (destinations, sliderName) => {
+    destinations.forEach((destination) => {
+        renderDestination(destination, sliderName);
+    });
+};
+
+/* Makes destinations list upon loading website using light slider library and jQuery*/
+$(document).ready(function () {
+    getDestinationElement(cityDestinations, "cities-slider");
+    $("#cities-slider").lightSlider({
+        autoWidth: true,
+        item: 3,
+        loop: true,
+        slideMove: 3,
+        easing: "cubic-bezier(0.25, 0, 0.25, 1)",
+        speed: 600,
+    });
+    getDestinationElement(beachDestinations, "beach-slider");
+    $("#beach-slider").lightSlider({
+        autoWidth: true,
+        item: 3,
+        loop: true,
+        slideMove: 3,
+        easing: "cubic-bezier(0.25, 0, 0.25, 1)",
+        speed: 600,
+    });
+});
