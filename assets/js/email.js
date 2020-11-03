94c4598e28fce99ffec58680ac5c935bf2c8cc85
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2020-11-03 19:22:17 +0000

    edited email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,17 +1,16 @@
-const sendMail = (contactForm) => {
-  emailjs
-    .send("service_v5eulln", "cont-for-m2", {
-      from_name: contactForm.name.value,
-      from_email: contactForm.emailaddress.value,
-      message: contactForm.message.value,
+function sendMail(contactForm) {
+    emailjs.send("brianwhelandublin","cont-for-m2", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value
     })
     .then(
-      function (response) {
-        alert("SUCCESS", response);
-      },
-      function (error) {
-        console.log("FAILED", error);
-      }
+        function(response) {
+            alert("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
     );
-    return false;
-};
+    return false;  // To block from loading a new page
+}
