1c49c891625916fefe64385eb852bdcc921642d4
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2020-11-05 18:03:06 +0000

    added emailjs to signup for newsletter section

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,18 +1,36 @@
-function sendMail(contactForm) {
-    emailjs.send("brianwhelandublin","cont-for-m2", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "message": contactForm.message.value
+function sendNewsletter(form) {
+  emailjs
+    .send("brianwhelandublin", "newsletter", {
+      "from_email": form.email.value,
     })
     .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            $('#myModal').modal({show:true})
-            contactForm.reset()
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
+      function (response) {
+        alert("SUCCESS", response);
+        form.reset();
+      },
+      function (error) {
+        alert("Something went wrong Please try again", error);
+      }
     );
-    return false;  // To block from loading a new page
+  return false;
+}
+
+function sendMail(contactForm) {
+  emailjs
+    .send("brianwhelandublin", "cont-for-m2", {
+      "from_name": contactForm.name.value,
+      "from_email": contactForm.email.value,
+      "message": contactForm.message.value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+        $("#myModal").modal({ show: true });
+        contactForm.reset();
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
+  return false; // To block from loading a new page
 }
