f33a01f3b1093f8f439e269a869459a0f655445d
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2020-11-10 11:27:48 +0000

    added the functions to add an event listener and show details to each marker

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -9,10 +9,13 @@ let currentInfoWindow;
 let service;
 let infoPane;
 let markers = [];
-let markerCluster
+let markerCluster;
 
 // Function to render map onto page for each destination area.
 const renderMap = (area) => {
+// to desplay info when markers are clicked
+  infoWindow = new google.maps.InfoWindow();
+  currentInfoWindow = infoWindow;
   const mapArea = {
     center: area,
     zoom: 10,
@@ -35,19 +38,18 @@ const placesSearch = (keyword) => {
 const callBack = (results, status) => {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     createMarkers(results);
-     markerCluster = new MarkerClusterer(map, markers, {
-    imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-  });
+    markerCluster = new MarkerClusterer(map, markers, {
+      imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
   } else {
     alert(
       "Something went wrong. Please contact us so we can look into this issue."
     );
   }
- 
 };
 
-/* Function to create markers for the search */
+/* Function to create markers for the search taken from the google documentation and google codelab tutorials*/
 
 const createMarkers = (places) => {
   places.forEach((place) => {
@@ -57,11 +59,47 @@ const createMarkers = (places) => {
       title: place.name,
       animation: google.maps.Animation.DROP,
     });
+    /* From stack overflow push markers into an array to be used in the markerClusterer function */
     markers.push(marker);
+    /* adds and event listener for each marker to show its details */
+    google.maps.event.addListener(marker, "click", () => {
+      let request = {
+        placeId: place.place_id,
+        fields: [
+          "name",
+          "formatted_address",
+          "geometry",
+          "rating",
+          "website",
+          "photos",
+        ],
+      };
+      service.getDetails(request, (placeResult, status) => {
+        showDetails(placeResult, marker, status);
+      });
+    });
   });
 };
 
-/* Event listeners for each booton to make the search */
+const showDetails = (placeResult, marker, status) => {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    let placeInfowindow = new google.maps.InfoWindow();
+    let rating = "None";
+    if (placeResult.rating) {
+      rating = placeResult.rating;
+    }
+    placeInfowindow.setContent(
+      `<div class="dark-text"><strong> ${placeResult.name} </strong><br> Rating:${rating}</div>`
+    );
+    placeInfowindow.open(marker.map, marker);
+    currentInfoWindow.close();
+    currentInfoWindow = placeInfowindow;
+  } else {
+    console.log("showDetails failed: " + status);
+  }
+};
+
+/* Event listeners for each button to make the search */
 document.getElementById("shopping-btn").addEventListener("click", () => {
   //event.preventDefault();
 
