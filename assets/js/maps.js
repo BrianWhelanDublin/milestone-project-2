78651b5642ac070c69113c6f682f7318506bb356
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2020-11-10 13:55:13 +0000

    added functionality to show the place results in panel

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -21,6 +21,8 @@ const renderMap = (area) => {
     zoom: 10,
   };
   map = new google.maps.Map(document.getElementById("map"), mapArea);
+  /* Adds map panel for search results*/
+  infoPane = document.getElementById('panel');
 };
 
 /* Function to search for different places in each area using Google places nearbySearch and a keyword */
@@ -84,7 +86,7 @@ const createMarkers = (places) => {
     });
   });
 };
-/* sows details of each place to the map infowindow */
+/* sows details of each place to the map info */
 const showDetails = (placeResult, marker, status) => {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     let placeInfowindow = new google.maps.InfoWindow();
@@ -93,11 +95,13 @@ const showDetails = (placeResult, marker, status) => {
       rating = placeResult.rating;
     }
     placeInfowindow.setContent(
-      `<div class="dark-text"><strong> ${placeResult.name} </strong><br> Rating:${rating}</div>`
+      `<div class="window-text"><strong> ${placeResult.name} </strong><br> Rating:${rating}</div>`
     );
     placeInfowindow.open(marker.map, marker);
     currentInfoWindow.close();
     currentInfoWindow = placeInfowindow;
+    /* calls showpanel function */
+    showPanel(placeResult)
   } else {
     console.log("showDetails failed: " + status);
   }
@@ -118,6 +122,61 @@ const removePreviousCluster = () =>{
         markerCluster.setMap(null)
     };
 };
+
+/* Function to show panel with details about each place */
+
+const showPanel = (placeResult) => {
+    let image = document.querySelector(".result-img-container");
+    let heading = document.querySelector(".result-heading");
+    let rating = document.querySelector(".rating");
+    let address = document.querySelector(".result-address");
+    let webAddress = document.querySelector(".web-address");
+/* closes infopane if it is open  */
+    if (infoPane.classList.contains("open")) {
+        infoPane.classList.remove("open");
+        infoPane.classList.add("hidden");
+    }
+    /* shows image if there is one otherwise lets user know that there was none found */
+    if (placeResult.photos) {
+        photo = placeResult.photos[0].getUrl();
+        image.innerHTML = `
+        <img src="${photo}" alt="image ${placeResult.name}" class="result-img">
+        `
+    } else {
+        image.innerHTML = `<p class="no-result"> No Image Available for this choice </p>`
+    }
+/* renders heading */
+    heading.innerHTML = `
+    <h3>${placeResult.name}</h3>
+    `
+    /* shows ratinf if there is one otherwise lets user know that there was none found */
+    if (placeResult.rating) {
+        rating.innerHTML = `
+        <p>Rating: ${placeResult.rating} \u272e</p>
+        `
+    } else {
+        rating.innerHTML = `
+        <p>No ratings found </p>
+        `
+    }
+    /* renders address */
+    address.innerHTML = `
+      <p>${placeResult.formatted_address}</p>
+      `
+      /* shows webaddress if there is one otherwise lets user know that there was none found */
+    if (placeResult.website) {
+        webAddress.innerHTML = `
+          <a href="${placeResult.website}">Web Page</a>
+          `
+    } else {
+        webAddress.innerHTML = `
+          <p>No web address found</p>
+          `
+    }
+    /* opens the infopane */
+    infoPane.classList.remove("hidden");
+    infoPane.classList.add("open");
+}
 
 /* Event listeners for each button to make the search */
 document.getElementById("shopping-btn").addEventListener("click", () => {
