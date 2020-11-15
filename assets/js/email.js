fd6141f80e68156845e93bbfb0e684c6649e3f1e
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2020-11-15 16:37:00 +0000

    added booking form email function

--- assets/js/email.js
+++ assets/js/email.js
@@ -10,7 +10,8 @@ function sendNewsletter(form) {
         form.reset();
       },
       function (error) {
-        alert("Something went wrong Please try again", error);
+        alert("Something went wrong Please try again");
+         console.log("FAILED", error);
       }
     );
   return false;
@@ -30,8 +31,31 @@ function sendMail(contactForm) {
         contactForm.reset();
       },
       function (error) {
+          alert("Opp's something has gone wrong Please try agian.")
         console.log("FAILED", error);
       }
     );
   return false; // To block from loading a new page
+}
+
+function sendBookingEnquiry(bookingForm){
+    emailjs.send("g-adventures2020", "booking-enquiry",{
+        "destination": bookingForm.destination-bf.value,
+        "package": bookingForm.package-bf.value,
+        "first_name": bookingForm.firstname-bf.value,
+        "surname": bookingForm.surname.value,
+        "from_email": bookingForm.emailaddress-bf.value,
+        "contact_no": bookingForm.phone-bf.value,
+        "address_line_one":bookingForm.inputaddress1-bf.value,
+        "address_line_two":bookingForm.inputaddress2-bf.value,
+        "city": bookingForm.inputcity-bf.value,
+        "province":bookingForm.inputeircode-bf.value,
+        "eircode" :bookingForm.inputaddress2-bf.value,
+        "no_of_adults":bookingForm.adults-bf.value,
+        "no_of_children":bookingForm.children-bf.value,
+        "arrival_date": bookingForm.arrival-bf.value,
+        "departure_date":bookingForm.depart-bf.value,
+        "requests":bookingForm.requests-bf.value,
+
+    })
 }
