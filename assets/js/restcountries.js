8284e257de6afc419b1232c0ccbee51a73517371
Brian Whelan <brian@friday.ie>
2022-03-26 17:57:17 +0000

    fix countries  api

--- assets/js/restcountries.js
+++ assets/js/restcountries.js
@@ -5,20 +5,20 @@ let flagArea = document.querySelector(".details-flag-col");
 let detailsList = document.querySelector(".details-list-col");
 
 // renders country flag on the page
-const renderFlag = (destination) =>{
-      flagArea.innerHTML = `
+const renderFlag = (destination) => {
+	flagArea.innerHTML = `
         <img src="${destination.flag}" alt="${destination.name} flag" class="flag center">`;
 };
 
 // renders message to site if theres an error finding the flag
-const renderFlagError = () =>{
-    flagArea.innerHTML = `
+const renderFlagError = () => {
+	flagArea.innerHTML = `
       <p class="error"> Error No flag found </p>`;
 };
 
 // renders list of country information onto page
-const renderDestinationInfoList = (destination) =>{
-     detailsList.innerHTML = `
+const renderDestinationInfoList = (destination) => {
+	detailsList.innerHTML = `
   	<ul class="details-list center">
       <li class="details-item">
          Country: ${destination.name}
@@ -41,31 +41,35 @@ const renderDestinationInfoList = (destination) =>{
 
 // renders message to site if theres an error finding the countries details
 const renderDestinationInfoListError = () => {
-  detailsList.innerHTML = `
+	detailsList.innerHTML = `
       <p class="error"> Error No Country details found </p>`;
 };
 
 // Api call to rest countries api using jQuery
 const getCountryData = (destination) => {
-  let country = destination.country;
-  $.when($.getJSON(`https://restcountries.eu/rest/v2/name/${country}`)).then(
-    (response) => {
-      let countryData = response[0];
-      renderDestinationInfoList(countryData);
-      renderFlag(countryData);
-    },
-    (errorResponse) => {
-      if (errorResponse.status === 404) {
-        renderFlagError();
-        renderDestinationInfoListError();
-      } else {
-          console.log(errorResponse);
-          flagArea.innerHTML=`
+	let country = destination.country;
+	$.when(
+		$.getJSON(`https://restcountries.com/v2/name/${country}?fullText=true
+  `)
+	).then(
+		(response) => {
+			console.log(response);
+			let countryData = response[0];
+			renderDestinationInfoList(countryData);
+			renderFlag(countryData);
+		},
+		(errorResponse) => {
+			if (errorResponse.status === 404) {
+				renderFlagError();
+				renderDestinationInfoListError();
+			} else {
+				console.log(errorResponse);
+				flagArea.innerHTML = `
           <p class ="error">${errorResponse.status}</p>
           `;
-        detailsList.innerHTML = `
+				detailsList.innerHTML = `
       <p class="error">${errorResponse.status}</p>`;
-      }
-    }
-  );
+			}
+		}
+	);
 };
